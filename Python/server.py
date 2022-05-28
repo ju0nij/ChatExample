8524c30ee37a573266e56e0e7ba5002137065f4d
Juyeong J <juyeong.j@outlook.com>
2022-05-28 18:03:19 +0900

    AES
    Testing

--- Python/server.py
+++ Python/server.py
@@ -1,4 +1,30 @@
+from hashlib import sha256
+from base64 import b64decode
+from base64 import b64encode
+
 import socket, threading
+import base64
+from Crypto import Random
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+
+class AES():
+    def __init__(self, key, iv):
+        self.key = sha256(key.encode()).digest()
+        # self.iv = iv.encode('utf-8')
+        self.iv = sha256(iv.encode()).digest()
+
+    def encrypt(self, plain):
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return bytes.decode(b64encode(cipher.encrypt(pad('abcd'.encode(), 
+            AES.block_size))))
+
+    def decrypt(self, cipher_text):
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return bytes.decode(unpad(cipher.decrypt(b64decode(cipher_text)), 
+            AES.block_size))
+
+aes = AES('abcdefgh12345678abcdefgh12345678', 'abcdefgh12345678')
 
 def client_receiver(client_socket, addr):
     
@@ -7,7 +33,7 @@ def client_receiver(client_socket, addr):
         while True:
             data = client_socket.recv(1024)
             msg = data.decode()
-            print('Received from', addr, msg)
+            print('Received from', addr, aes.decrypt(msg).encode())
             data = msg.encode()
             client_socket.sendall(data)
     except:
